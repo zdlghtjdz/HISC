433870ce470fa1e97114bfda901bf72d4587102f
khl2bb <43461931+khl2bb@users.noreply.github.com>
2019-07-14 23:28:14 -0400

    Add files via upload

---
+++ background2.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cv2 as cv
+cap = cv.VideoCapture('flyingtest1.MOV')
+fgbg = cv.createBackgroundSubtractorMOG2()
+while(1):
+    ret, frame = cap.read()
+    fgmask = fgbg.apply(frame)
+    cv.imshow('frame',fgmask)
+    k = cv.waitKey(30) & 0xff
+    if k == 27:
+        break
+cap.release()
+cv.destroyAllWindows()
